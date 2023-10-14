fee26429dc7b912e41c6f58389f9e437d4660171
carlosgrprr <carlosgrprr@gmail.com>
2023-10-14 15:01:28 +0200

    Server and web sockets

---
+++ app/p2p-server.js
@@ -0,0 +1,24 @@
+const Websocket =require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+class P2pServer{
+    contructor(blockchain) {
+        this.blockchain = blockchain;
+        this.sockets = [];
+    }
+
+    listen() {
+        const server = new Websocket.Server({ port: P2P_PORT });
+        server.on('connection', socket => this.connectSocket(socket));
+        console.log(`Listening for peer-to-peer connection on: ${P2P_PORT}`);
+    }
+
+    connectSocket(socket) {
+        this.sockets.push(socket);
+        console.log('Socket connected');
+    }
+}
+
+/* $ HTTP _PORT=3002 P2P_PORT=5003 PERRS=ws://localhost:5001,ws://localhost:5002 npm run dev */
