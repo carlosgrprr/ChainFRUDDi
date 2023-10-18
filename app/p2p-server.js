08d368ac13fcfb454862e13a8c81e4ff99f2da95
carlosgrprr <carlosgrprr@gmail.com>
2023-10-18 23:08:28 +0200

    P2P server

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,10 +1,10 @@
-const Websocket =require('ws');
+const Websocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 class P2pServer {
-    contructor(blockchain) {
+    constructor(blockchain) {
         this.blockchain = blockchain;
         this.sockets = [];
     }
@@ -12,13 +12,39 @@ class P2pServer {
     listen() {
         const server = new Websocket.Server({ port: P2P_PORT });
         server.on('connection', socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+
         console.log(`Listening for peer-to-peer connection on: ${P2P_PORT}`);
+    }
+
+    connectToPeers () {
+        peers.forEach(peer => {
+            const socket = new Websocket(peer);
+
+            socket.on('open', () => this.connectSocket(socket));
+        });
     }
 
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHandler(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    messageHandler(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data', data);
+        });
     }
 }
 
+module.exports = P2pServer; 
+
+
+// ws://localhost:5001 It is how a peer looks like
 /* $ HTTP _PORT=3002 P2P_PORT=5003 PERRS=ws://localhost:5001,ws://localhost:5002 npm run dev */
